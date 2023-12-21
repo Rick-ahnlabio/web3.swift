ea3d1ebdf3f392343c52690cd892ed3c114ab00c
rick-abc <rick@ahnlab.io>
2023-12-21 18:51:16 +0900

    feat: Modified to apply http header to http provider

--- web3swift/src/Client/NetworkProviders/HttpNetworkProvider.swift
+++ web3swift/src/Client/NetworkProviders/HttpNetworkProvider.swift
@@ -12,10 +12,11 @@ import Foundation
 public class HttpNetworkProvider: NetworkProviderProtocol {
     public let session: URLSession
     private let url: URL
-
-    public init(session: URLSession, url: URL) {
+    private let headers: [String: String]
+    public init(session: URLSession, url: URL, headers: [String: String]? = nil) {
         self.session = session
         self.url = url
+        self.headers = headers ?? [:]
     }
 
     deinit {
@@ -32,7 +33,11 @@ public class HttpNetworkProvider: NetworkProviderProtocol {
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
-
+        
+        self.headers.forEach { key, value in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        
         let id = 1
         let rpcRequest = JSONRPCRequest(jsonrpc: "2.0", method: method, params: params, id: id)
         guard let encoded = try? JSONEncoder().encode(rpcRequest) else {
